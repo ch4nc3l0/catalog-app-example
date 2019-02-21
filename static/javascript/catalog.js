146ce203aeba118bd8ed86cc1029791a4c212e5a
chance <ch4nc3l0@gmail.com>
2019-02-21 05:52:52 -0500

    Fix: catalog.js incorrectly loading

--- static/javascript/catalog.js
+++ static/javascript/catalog.js
@@ -1,15 +1,19 @@
-let user = '{{user}}';
+
         if (user != ''){
             document.getElementById('loginButt').style.display = 'none';
             document.getElementById('logoutButt').style.display = '';
             document.getElementById('newCat').style.display = '';
-            document.getElementById('editCat').style.display = '';
-            document.getElementById('delCat').style.display = '';
+            if(categories != ''){
+                document.getElementById('editCat').style.display = '';
+                document.getElementById('delCat').style.display = '';
+            }
         }
         else{
             document.getElementById('logoutButt').style.display = 'none';
             document.getElementById('loginButt').style.display = '';
             document.getElementById('newCat').style.display = 'none';
-            document.getElementById('editCat').style.display = 'none';
-            document.getElementById('delCat').style.display = 'none';
+            if(categories != ''){
+                document.getElementById('editCat').style.display = 'none';
+                document.getElementById('delCat').style.display = 'none';
+            }
         }
